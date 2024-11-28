56f48d48ca507719c2f0962375afc9f5bf530805
Miguel Ángel Ávila Rosas <148963870+MiguelAngelAvilaRosas1DAW@users.noreply.github.com>
2024-11-28 20:58:21 +0100

    Delete libs directory

--- libs/conectarBD.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//Nos conectamos a la base de datos.
-try {
-    $pdo = new PDO("mysql:host=db;dbname=bdJuegosRetro;charset=utf8mb4", "root", "");
-} catch (PDOException $exception) {
-    $errorUsuario = "No se pudo conectar a la BD.";
-}
